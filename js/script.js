3744578f06f26eebe410334ac046cc1928d15bb2
Srivathsan P <156175839+SrivathsanP23@users.noreply.github.com>
2024-12-31 01:07:11 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ menuIcon.onclick = () => {
 
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('div nav a');
 
 window.onscroll = () => {
 
@@ -20,10 +20,10 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top>= offset  && top< offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+                document.querySelector('div nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
@@ -36,36 +36,36 @@ window.onscroll = () => {
 
 
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-    
-        menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
-    
-    
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
 
 };
 
 
 /*==================== scroll reveal ====================*/
-ScrollReveal({ 
+ScrollReveal({
     // reset: true,
-    distance: '80px',
+    distance: '90px',
     duration: 3000,
-    delay:100 
+    delay: 100
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box',{ origin: 'bottom' });
+ScrollReveal().reveal('.services-container, .portfolio-box', { origin: 'bottom' });
 
 
-ScrollReveal().reveal('.home-content h1, .about-img',{ origin: 'left' });
+ScrollReveal().reveal('.home-content h1, .home-img', { origin: 'left' });
 
-ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content,.experience .heading ', { origin: 'right' });
 
-ScrollReveal().reveal('.education .heading',{ origin: 'left' });
+ScrollReveal().reveal('.education .heading', { origin: 'left' });
 
-ScrollReveal().reveal('.timeline-item',{ origin: 'bottom' });
+ScrollReveal().reveal('.timeline-item , .extimeline-item', { origin: 'bottom' });
 
-ScrollReveal().reveal('.education-title',{ origin: 'right' });
+ScrollReveal().reveal('.education-title', { origin: 'right' });
 
 
 
@@ -74,10 +74,10 @@ ScrollReveal().reveal('.education-title',{ origin: 'right' });
 /*==================== typed js animation ====================*/
 
 
-const typed = new Typed('.multiple-text',{
-    strings: ['Web Developer', 'Data Analyst'],
-    typeSpeed: 130,
-    backSpeed: 90,
-    backDelay: 1000,
+const typed = new Typed('.multiple-text', {
+    strings: ['Data Analyst', 'Web Developer'],
+    typeSpeed: 305,
+    backSpeed: 190,
+    backDelay: 700,
     loop: true
 });
